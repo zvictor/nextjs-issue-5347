886a87fa352490f1de95d20c47577f4b412df58b
zVictor <victor0110@msn.com>
2019-01-25 10:57:52 +0100

    add apollo to it

--- components/outer.js
+++ components/outer.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
+import styled from '@emotion/styled'
+
+const Div = styled.div`
+  width: 400px;
+  margin: auto;
+`
 
 const Inner = dynamic(import('../components/inner'))
 
-export default () => <div>Outer Component <hr /><Inner /></div>
+export default () => <Div>Outer Component <hr /><Inner /></Div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 
-const Component = dynamic(import('../components/outer'))
+// const Component = dynamic(import('../components/outer'))
 // const Component = dynamic(import('../components/inner'))
 
 export default () => {
-  // const Component = dynamic(import('../components/outer'))
+  const Component = dynamic(import('../components/outer'))
   // const Component = dynamic(import('../components/inner'))
 
   return (
